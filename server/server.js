68720f0bfe5804418f2612394a6976ec7a57049d
Pavel Solovev <pashius2010@gmail.com>
2021-05-22 17:34:38 +0400

    buildin backend

--- server/server.js
+++ server/server.js
@@ -1,39 +1,21 @@
-import path from "path";
-import express from "express";
-import devBundle from "./devBundle";
-import template from "./../template";
-//comment out before building for production
-import { MongoClient } from "mongodb";
+import config from "./../config/config";
+import app from "./express";
+import mongoose from "mongoose";
 
-const app = express();
-
-//comment out before building for production
-devBundle.compile(app);
-
-const CURRENT_WORKING_DIR = process.cwd();
-app.use("/dist", express.static(path.join(CURRENT_WORKING_DIR, "dist")));
-
-app.get("/", (req, res) => {
-  res.status(200).send(template());
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoUri, {
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useNewUrlParser: true,
 });
 
-let port = process.env.PORT || 3000;
-app.listen(port, function onStart(err) {
+mongoose.connection.on("error", () => {
+  throw new Error(`unable to connect to database: ${mongoUri}`);
+});
+
+app.listen(config.port, (err) => {
   if (err) {
     console.log(err);
   }
-  console.info("Server started on port %s.", port);
+  console.info("Server started on port %s.", config.port);
 });
-
-// Database Connection URL
-const url =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/mernSimpleSetup";
-// Use connect method to connect to the server
-MongoClient.connect(
-  url,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err, db) => {
-    console.log("Connected successfully to mongodb server");
-    db.close();
-  }
-);
